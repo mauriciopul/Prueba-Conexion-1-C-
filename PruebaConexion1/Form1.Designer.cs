5eeb03a63319f32f1172876a40153cd08f148c6d
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-08-22 00:11:05 -0500

    Se crea la estructura visual del formulario

--- PruebaConexion1/Form1.Designer.cs
+++ PruebaConexion1/Form1.Designer.cs
@@ -28,13 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtAgregar = new System.Windows.Forms.RadioButton();
+            this.rbtModificar = new System.Windows.Forms.RadioButton();
+            this.rbtEliminar = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.tctNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.dtFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.rbtEliminar);
+            this.groupBox1.Controls.Add(this.rbtModificar);
+            this.groupBox1.Controls.Add(this.rbtAgregar);
+            this.groupBox1.Location = new System.Drawing.Point(32, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(454, 73);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CRUD Prueba de Conexión 1";
+            // 
+            // rbtAgregar
+            // 
+            this.rbtAgregar.AutoSize = true;
+            this.rbtAgregar.Location = new System.Drawing.Point(44, 29);
+            this.rbtAgregar.Name = "rbtAgregar";
+            this.rbtAgregar.Size = new System.Drawing.Size(62, 17);
+            this.rbtAgregar.TabIndex = 0;
+            this.rbtAgregar.TabStop = true;
+            this.rbtAgregar.Text = "Agregar";
+            this.rbtAgregar.UseVisualStyleBackColor = true;
+            // 
+            // rbtModificar
+            // 
+            this.rbtModificar.AutoSize = true;
+            this.rbtModificar.Location = new System.Drawing.Point(180, 29);
+            this.rbtModificar.Name = "rbtModificar";
+            this.rbtModificar.Size = new System.Drawing.Size(68, 17);
+            this.rbtModificar.TabIndex = 1;
+            this.rbtModificar.TabStop = true;
+            this.rbtModificar.Text = "Modificar";
+            this.rbtModificar.UseVisualStyleBackColor = true;
+            // 
+            // rbtEliminar
+            // 
+            this.rbtEliminar.AutoSize = true;
+            this.rbtEliminar.Location = new System.Drawing.Point(322, 29);
+            this.rbtEliminar.Name = "rbtEliminar";
+            this.rbtEliminar.Size = new System.Drawing.Size(61, 17);
+            this.rbtEliminar.TabIndex = 2;
+            this.rbtEliminar.TabStop = true;
+            this.rbtEliminar.Text = "Eliminar";
+            this.rbtEliminar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fecha de Nacimiento:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(166, 113);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(129, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // tctNombre
+            // 
+            this.tctNombre.Location = new System.Drawing.Point(166, 144);
+            this.tctNombre.Name = "tctNombre";
+            this.tctNombre.Size = new System.Drawing.Size(129, 20);
+            this.tctNombre.TabIndex = 6;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(166, 175);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(129, 20);
+            this.txtApellidos.TabIndex = 7;
+            // 
+            // dtFechaNacimiento
+            // 
+            this.dtFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFechaNacimiento.Location = new System.Drawing.Point(166, 203);
+            this.dtFechaNacimiento.Name = "dtFechaNacimiento";
+            this.dtFechaNacimiento.Size = new System.Drawing.Size(129, 20);
+            this.dtFechaNacimiento.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(411, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 248);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(454, 188);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(515, 459);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtFechaNacimiento);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.tctNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Prueba de Conexion 1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtEliminar;
+        private System.Windows.Forms.RadioButton rbtModificar;
+        private System.Windows.Forms.RadioButton rbtAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox tctNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.DateTimePicker dtFechaNacimiento;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
